d92c6eae6c1add16437e1a80da5cef94c1e8e686
Mark Thomson <mark@kimbolton>
2020-08-28 18:18:58 +1200

    Testing

--- actionpack/lib/action_dispatch/testing/integration.rb
+++ actionpack/lib/action_dispatch/testing/integration.rb
@@ -287,7 +287,7 @@ module ActionDispatch
             "REMOTE_ADDR"    => remote_addr,
             "CONTENT_TYPE"   => "application/x-www-form-urlencoded",
             "HTTP_ACCEPT"    => accept,
-            "SCRIPT_NAME"    => nil
+            "SCRIPT_NAME"    => '/pre'
           }
           # this modifies the passed env directly
           Http::Headers.new(env).merge!(headers_or_env || {})
